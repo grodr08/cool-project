0198da8525afbe57f216db22a8198dd2c38e9cd4
Giancarlos Rodriguez <91751980+grodr08@users.noreply.github.com>
2023-08-30 01:15:50 +0100

    added the Sieve of Eratosthenes algorithm

--- hello_students.py
+++ hello_students.py
@@ -1,3 +1,17 @@
-print('hello world')
-print('I really hope you folks like the lecture')
-print('This is from the vim editor')
+from math import isqrt
+
+def primes_less_than(n: int) -> list[int]:
+    if n<= 2: 
+        return []
+    is_prime = [True] * n
+    is_prime[0] = False
+    is_prime[1] = False
+
+    for i in range(2, isqrt(n)):
+        for j in range(i*i, n, i):
+            is_prime[j] = False
+    return [i for i in range(n) if is_prime[i]]
+
+
+if __name__ == '__main__':
+    print(primes_less_than(100))
